d4bb294e87c870f236c3dcd3dd46ee4d0cfc634d
snamiki1212 <snamiki1212@gmail.com>
2024-08-24 17:04:02 +0900

    fix: update lo

--- src/lo/main.go
+++ src/lo/main.go
@@ -9,4 +9,17 @@ import (
 func main() {
 	names := lo.Uniq([]string{"Samuel", "John", "Samuel"})
 	fmt.Println(names)
+	us := Users{{UserID: "1", Name: "John"}, {UserID: "2", Name: "Taro"}}
+	us = lo.Map(us, func(u *User, _ int) *User {
+		u.Name = u.Name + "-san"
+		return u
+	})
+	fmt.Println(us)
 }
+
+type User struct {
+	UserID string
+	Name   string
+}
+
+type Users []*User
