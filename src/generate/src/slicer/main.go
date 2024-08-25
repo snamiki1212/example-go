21de805e86495d0efa397135684989fd2e10064b
snamiki1212 <snamiki1212@gmail.com>
2024-08-26 00:14:18 +0900

    fix: update gen

--- src/generate/src/slicer/main.go
+++ src/generate/src/slicer/main.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"slices"
 )
 
 // const templatePath = "domain/user/gen/getter.tmpl"
@@ -13,69 +14,119 @@ const inputPath = "./src/core/domain/user/user.go"
 const outputPath = "./src/core/domain/user/user.gen.go"
 const inputFile = "user.go"
 const outputFile = "user_gen.go"
+const entityName = "User"
 
-// func main() {
-// 	// inputPath := "../user.go"
-// 	path, err := os.Getwd()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println(path)
-
-// 	// output file
-// 	file, err := os.Create(outputPath)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer file.Close()
-
-// 	fset := token.NewFileSet()
-// 	node, err := parser.ParseFile(fset, inputPath, nil, parser.AllErrors)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	ast.Print(fset, node)
-
-// 	// // template
-// 	// t := template.Must(template.ParseFiles(templatePath))
-// 	// replaceRule := map[string]string{
-// 	// 	"REPLACE_ME": ":D",
-// 	// } //
-// 	// if err := t.Execute(file, replaceRule); err != nil {
-// 	// 	panic(err)
-// 	// }
-// 	fmt.Println("generate!!!")
-// }
-
-// func Main() {
-// 	main()
-// }
+const isDebug = true // TODO:
 
 func main() {
 	fmt.Println("this is slicer")
+	excludeFields := []string{"Posts"}
 
 	// args := os.Args
-	// entityName := "User"
+
 	// sliceName := "Users"
 
 	// pwd
-	pwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(pwd)
+	pwd := getPWD()
 
-	// output file
+	// Output file
 	file, err := os.Create(pwd + "/" + outputFile)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	// Ast
 	fset := token.NewFileSet()
 	node, err := parser.ParseFile(fset, pwd+"/"+inputFile, nil, parser.AllErrors)
 	if err != nil {
 		panic(err)
 	}
-	ast.Print(fset, node)
+	if isDebug {
+		ast.Print(fset, node) // for debug
+	}
+
+	{
+		objs := node.Scope.Objects
+		obj, ok := objs[entityName]
+		if !ok {
+			panic("not found entity")
+		}
+
+		fmt.Println(obj.Name)
+		fmt.Println(obj.Kind)
+		decl := obj.Decl
+		fmt.Println(decl)
+
+		// switch de := decl.(type) {
+		// case *ast.TypeSpec:
+		// 	fmt.Println("TypeSpec", de)
+		// default:
+		// 	panic("invalid decl")
+		// }
+		entity, ok := decl.(*ast.TypeSpec)
+		if !ok {
+			panic("invalid decl")
+		}
+		fmt.Println(entity)
+		ty := entity.Type
+
+		// https://stackoverflow.com/questions/20234342/get-a-simple-string-representation-of-a-struct-field-s-type
+		sty, ok := ty.(*ast.StructType)
+		if !ok {
+			panic("invalid type")
+		}
+		rawFields := sty.Fields.List
+		fmt.Println(rawFields)
+
+		fields := newFields(rawFields)
+		fmt.Println(fields)
+		fields = fields.exclude(excludeFields)
+		fmt.Println(fields)
+		// DEBUG
+		fmt.Println(":D")
+
+	}
+
+}
+
+func getPWD() string {
+	if isDebug {
+		return "/Users/snamiki1212/ghq/github.com/snamiki1212/example-go/src/generate/src/core/domain/user"
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pwd)
+	return pwd
+}
+
+type (
+	Fields []Field
+	Field  struct {
+		Name string
+		Type string
+	}
+)
+
+func newField(raw *ast.Field) Field {
+	return Field{
+		Name: raw.Names[0].Name,
+		Type: raw.Type.(*ast.Ident).Name,
+	}
+}
+
+func newFields(raws []*ast.Field) Fields {
+	fields := make(Fields, 0, len(raws))
+	for _, raw := range raws {
+		fields = append(fields, newField(raw))
+	}
+	return fields
+}
+
+func (fs Fields) exclude(targets []string) Fields {
+	return slices.DeleteFunc(fs, func(f Field) bool {
+		return slices.Contains(targets, f.Name)
+	})
 }
